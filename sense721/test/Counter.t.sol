e5c513d81474aaf6a005e3ad5f5d5e34eaf8a3fd
Augustin Villetard <augustin.dev@tutamail.com>
2025-04-15 14:25:51 +0900

    test: test nft contract

--- sense721/test/Counter.t.sol
+++ sense721/test/Counter.t.sol
@@ -1,24 +1,63 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {Test, console2} from "forge-std/Test.sol";
+import {DreamNFT} from "../src/DreamNFT.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
-
+contract DreamNFTTest is Test {
+    DreamNFT public nft;
+    
+    uint256 ownerPrivateKey = 0x1; 
+    uint256 userPrivateKey = 0x2; 
+    
+    // Derive addresses from these keys
+    address owner;
+    address user;
+    
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        // Set up addresses from private keys
+        owner = vm.addr(ownerPrivateKey);
+        user = vm.addr(userPrivateKey);
+        
+        // Deploy contract with owner
+        vm.startPrank(owner);
+        nft = new DreamNFT("https://test-uri.com/");
+        vm.stopPrank();
     }
-
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+    
+    function testMintDream() public {
+        // Only owner can mint directly
+        vm.startPrank(owner);
+        
+        string memory tokenURI = "ipfs://Qm123456789";
+        uint256 tokenId = nft.mintDream(user, tokenURI);
+        
+        // Verify token ownership and metadata
+        assertEq(nft.ownerOf(tokenId), user);
+        assertEq(nft.tokenURI(tokenId), tokenURI);
+        
+        vm.stopPrank();
     }
-
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    
+    function testMintWithSignature() public {
+        string memory tokenURI = "ipfs://Qm987654321";
+        
+        // Create message hash from user address and tokenURI
+        bytes32 messageHash = keccak256(abi.encodePacked(user, tokenURI));
+        bytes32 ethSignedMessageHash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash));
+        
+        // Owner signs the message with the ACTUAL owner private key
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, ethSignedMessageHash);
+        bytes memory signature = abi.encodePacked(r, s, v);
+        
+        // User mints with signature
+        vm.startPrank(user);
+        uint256 tokenId = nft.mintWithSignature(tokenURI, signature);
+        
+        // Verify token ownership and metadata
+        assertEq(nft.ownerOf(tokenId), user);
+        assertEq(nft.tokenURI(tokenId), tokenURI);
+        
+        vm.stopPrank();
     }
 }
